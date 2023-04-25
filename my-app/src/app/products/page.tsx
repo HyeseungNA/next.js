5114b589d45fe1c7354034fd876be277628e8599
나혜승 <nahs00122@gmail.com>
2023-04-25 14:43:46 +0900

    fetch를 사용한 SSG, ISR, SSR

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import { getProducts } from "@/service/products";
+import styles from './page.module.css'
 
 export const revalidate = 3;
 
-export default async  function ProductsPage() {
+export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서 보여줌
   const products = await getProducts();
+  const res = await fetch("https://meowfacts.herokuapp.com",{
+    // next: {revalidate:0},
+    cache:'no-store'
+  });
+  const data = await res.json();
+  const factText = data.data[0];
   return (
     <>
       <h1>제품 소개 페이지</h1>
@@ -16,6 +23,7 @@ export default async  function ProductsPage() {
           </li>
         ))}
       </ul>
+      <article className={styles.article} >{factText}</article>
     </>
   );
 }

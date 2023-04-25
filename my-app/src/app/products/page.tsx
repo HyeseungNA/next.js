de1f4b0d925e6919cf83afc601eda5d23ce35630
나혜승 <nahs00122@gmail.com>
2023-04-25 15:03:04 +0900

    fetch를 이용한 csr

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,18 +1,15 @@
 import Link from "next/link";
 import { getProducts } from "@/service/products";
-import styles from './page.module.css'
+// import styles from './page.module.css'
+import MeowArticle from "../components/MeowArticle";
 
 export const revalidate = 3;
 
 export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서 보여줌
   const products = await getProducts();
-  const res = await fetch("https://meowfacts.herokuapp.com",{
-    // next: {revalidate:0},
-    cache:'no-store'
-  });
-  const data = await res.json();
-  const factText = data.data[0];
+ 
+
   return (
     <>
       <h1>제품 소개 페이지</h1>
@@ -23,7 +20,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
-      <article className={styles.article} >{factText}</article>
+     <MeowArticle />
     </>
   );
 }

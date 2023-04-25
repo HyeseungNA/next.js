26f4f2dd3d23234d7622e4568cd80406c6d61fa3
나혜승 <nahs00122@gmail.com>
2023-04-26 00:35:37 +0900

    이미지 실습

--- my-app/src/app/products/[slug]/page.tsx
+++ my-app/src/app/products/[slug]/page.tsx
@@ -1,35 +1,45 @@
 import { getProduct, getProducts } from "@/service/products";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 type Props = {
   params: {
     slug: string;
-  }
-}
+  };
+};
 
 export const revalidate = 3;
 
-export function generateMetadata({params:{slug}}:Props) {
-  
+export function generateMetadata({ params: { slug } }: Props) {
   return {
     title: `제품의 이름: ${slug}`,
-  }
+  };
 }
 
-export default async function ProductPage({params:{slug}}:Props) {
+export default async function ProductPage({ params: { slug } }: Props) {
   const product = await getProduct(slug);
-  if(!product) {
+  if (!product) {
     notFound();
   }
   //서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product.name} 제품 설명 페이지</h1>
+  return(
+  <>
+    <h1>{product.name} 제품 설명 페이지</h1>
+    <Image 
+    src= {`/images/${product.image}`}
+  alt={product.name}
+  width='300'
+  height='300'
+  />
+  </>
+  );
 }
 
 // 미리 만들어 놓는 것
 export async function generateStaticParams() {
   // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임(SSG)
   const products = await getProducts();
-  return products.map(product => ({
-    slug: product.id
-  }))
+  return products.map((product) => ({
+    slug: product.id,
+  }));
 }

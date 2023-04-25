da22aacfc76f905d14ea9b56e5d94fb0301cc87f
나혜승 <nahs00122@gmail.com>
2023-04-25 10:09:03 +0900

    SEO 추가

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
+// import { Inter } from 'next/font/google'
 import { notFound } from "next/navigation";
 
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   // notFound();

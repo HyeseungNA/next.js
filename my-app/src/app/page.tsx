56e0512224b262d3626066fd85057b9904ea5e4f
나혜승 <nahs00122@gmail.com>
2023-04-26 00:24:17 +0900

    이미지 활용

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 
 import os from "os"; //노드 APIs
 import Counter from "./components/Counter";
+import Image from "next/image";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,10 @@ export default function Home() {
     <>
     <h1>홈페이지</h1>
     <Counter/>
+    <Image src = 'https://images.unsplash.com/photo-1441986300917-64674bd600d8'
+    alt = 'shop'
+    width={400}
+    height={400}/>
     </>
   );
 }
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -2,18 +2,21 @@ import Link from "next/link";
 import { getProducts } from "@/service/products";
 // import styles from './page.module.css'
 import MeowArticle from "../components/MeowArticle";
+import clothesImage from '../../../public/Images/cloth.jpg'
+import Image from "next/image";
 
 export const revalidate = 3;
 
 export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서 보여줌
   const products = await getProducts();
-  throw new Error();
  
-
   return (
     <>
       <h1>제품 소개 페이지</h1>
+      <Image src={clothesImage} priority />
+
+
       <ul>
         {products.map((product, index) => (
           <li key={index}>
